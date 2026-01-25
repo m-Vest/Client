594e761571a6ad6dbe84c16a484f862db9eb6a68
jllee000 <jllee000@naver.com>
2026-01-25 23:25:39 +1100

    feat: z-index 올리기

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,7 +4,7 @@ const Layout = () => {
   return (
     <div className="min-h-dvh w-full bg-black flex justify-center">
         <main className="relative w-[100vw] max-w-[500px] bg-white min-h-dvh">
-            <div className="absolute bottom-0 left-0 right-0">
+            <div className="absolute bottom-0 left-0 right-0 z-10">
                 <TabBar />
             </div>
             <Outlet />
