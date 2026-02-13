e5ed156c462d683e5d2b6cbba80f2b3c9b7ce423
jllee000 <jllee000@naver.com>
2026-02-13 16:09:21 +0900

    refactor: 카카오로그인 훅으로 분리

--- src/pages/login/auth/KakaoLogin.tsx
+++ src/pages/login/auth/KakaoLogin.tsx
@@ -1,53 +1,15 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { getKakaoAccessToken } from '../../../apis/auth';
-import { ROUTES_CONFIG } from '../../../routes/routesConfig';
-import { api } from '../../../lib/api';
-import { nav } from 'framer-motion/client';
+import { useKakaoLogin } from '../../../apis/query/useKakaoLogin';
 const KakaoRedirect = () => {
-  const navigate = useNavigate();
+   const { loginWithKakao } = useKakaoLogin();
 
   useEffect(() => {
-    const handleLogin = async () => {
-      try {
-        const code = new URL(window.location.href).searchParams.get('code');
+    const code = new URL(window.location.href).searchParams.get('code');
 
-        if (!code) {
-          throw new Error('카카오 인가 코드 없음');
-        }
-        const kakaoToken = await getKakaoAccessToken(code);
-        localStorage.setItem('kakaoAccessToken', kakaoToken.access_token);
-        const { data } = await api.post(
-          '/auth/login',
-          { platform: 'KAKAO' },
-          {
-            headers: {
-              Authorization: `Bearer ${kakaoToken.access_token}`,
-            },
-        }
-        );
-        console.log(data.data.type)
-        if (data.data.type === "SIGNUP_REQUIRED"){
-          localStorage.setItem('signupToken', data.data.signupToken);
-          navigate(ROUTES_CONFIG.onboarding.path);
-        } else {
-          if (data.data.jwtToken){
-            localStorage.setItem('accessToken', data.data.jwtToken);
-            navigate(ROUTES_CONFIG.home.path);
-          }
-        }
-
-        navigate(ROUTES_CONFIG.onboarding.path);
-
-      } catch (error) {
-        console.error('카카오 로그인 실패', error);
-        navigate(ROUTES_CONFIG.login.path);
-      }
-    };
-
-    handleLogin();
-  }, [navigate]);
-
+    if (code) {
+      loginWithKakao(code);
+    }
+  }, []);
 
 
   return (
