36d535a27a86ffa44ba019830e0bc48290dc4802
jllee000 <jllee000@naver.com>
2026-02-19 23:45:36 +0900

    feat: 헤더 하단 그림자 추가

--- src/pages/order/components/OrderHeader.tsx
+++ src/pages/order/components/OrderHeader.tsx
@@ -1,7 +1,7 @@
 import BackIcon from '/icons/order/back.svg'
 const OrderHeader = () =>{
     return (
-        <div className="absolute z-10 top-0 left-0 w-full bg-white px-[1.2rem] py-[0.8rem] flex flex-row justify-start gap-[1.2rem] items-center">
+        <div className="absolute z-10 top-0 left-0 w-full bg-white px-[1.2rem] py-[0.8rem] flex flex-row justify-start gap-[1.2rem] items-center border-b border-b-[#c9ced5]">
             <img src={BackIcon} className="w-[4rem] h-[4rem]"/>
             <span className='text-[1.7rem] font-bold'>SK 하이닉스</span>
         </div>
