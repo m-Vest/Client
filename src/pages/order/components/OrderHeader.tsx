0f84fe69e2df75ccc6dd58641ede741efa1fc78a
jllee000 <jllee000@naver.com>
2026-02-19 23:46:58 +0900

    feat: 리스트로 뒤로가기 헤더

--- src/pages/order/components/OrderHeader.tsx
+++ src/pages/order/components/OrderHeader.tsx
@@ -1,8 +1,10 @@
 import BackIcon from '/icons/order/back.svg'
+import { useNavigate } from 'react-router-dom';
 const OrderHeader = () =>{
+    const navigate = useNavigate();
     return (
         <div className="absolute z-10 top-0 left-0 w-full bg-white px-[1.2rem] py-[0.8rem] flex flex-row justify-start gap-[1.2rem] items-center border-b border-b-[#c9ced5]">
-            <img src={BackIcon} className="w-[4rem] h-[4rem]"/>
+            <img src={BackIcon} className="w-[4rem] h-[4rem]" onClick={()=> navigate('/list')}/>
             <span className='text-[1.7rem] font-bold'>SK 하이닉스</span>
         </div>
     )
