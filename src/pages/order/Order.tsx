1b4e64ae47259d6468944e17f12fff8e88c5f638
jllee000 <jllee000@naver.com>
2026-02-19 23:44:29 +0900

    feat: 거래 정보

--- src/pages/order/Order.tsx
+++ src/pages/order/Order.tsx
@@ -2,20 +2,20 @@ import OrderHeader from "./components/OrderHeader";
 import walletImg from '/icons/order/wallet.png'
 const Order = () =>{
     return (
-        <div className="relative w-full h-full flex flex-col gap-[2.4rem] justify-between bg-[#F9FAFB]">
+        <div className="relative w-full h-full flex flex-col gap-[2.4rem] justify-between bg-[#fafafa]">
             <OrderHeader />
-            <div className="relative pt-[10rem] px-[2rem] flex flex-col gap-[1.6rem]">
+            <div className="relative pt-[8rem] px-[2rem] pb-[11rem] flex flex-col gap-[1.6rem] bg-[#fafafa]">
                 <div className="rounded-3xl bg-white shadow-sm p-[2.4rem]">
                      <h1 className="text-[#6A7282] text-[1.4rem] font-normal">현재가</h1>
                      <p className=" mb-[1.2rem] text-[3.6rem] font-bold">123,456 <span className="text-[2.4rem]">원</span></p>
                      <span className="py-[0.65rem] text-[1.4rem] font-semibold bg-[#EFF6FF] text-[#155DFC] px-[1.2rem] rounded-full">-1,950원 (-1.5%)</span>
                 </div>
-                 <div className="rounded-3xl p-[2.5rem] flex flex-col border border-[#DBEAFE] bg-gradient-to-br from-[#EFF6FF] to-[#EEF2FF]">
+                 <div className="rounded-3xl p-[2rem] flex flex-col border border-[#DBEAFE] bg-gradient-to-br from-[#EFF6FF] to-[#EEF2FF]">
                     <div className="flex flex-row gap-[0.8rem] items-center justify-start mb-[2rem]">
-                        <img src={walletImg} className="w-auto h-[4rem]"/>
+                        <img src={walletImg} className="w-auto h-[3rem]"/>
                         <p className="text-[1.6rem] font-bold">내 보유 정보</p>
                     </div>
-                    <div className="flex flex-col gap-[1.4rem] border-b border-b-[#BEDBFF] pb-[1.4rem]">
+                    <div className="flex flex-col gap-[1.2rem] border-b border-b-[#BEDBFF] pb-[1.4rem]">
                         <div className="flex flex-row justify-between w-full items-center">
                             <span className="text-[#4A5565] text-[1.4rem] font-normal">보유 수량</span>
                             <span className="text-black text-[1.6rem] font-bold">1주</span>
@@ -34,8 +34,28 @@ const Order = () =>{
                         <span className="text-[#E7000B] text-[1.8rem] font-bold">+0원(+0.00%)</span>
                     </div>
                 </div>
+                <div className="p-[2rem] rounded-3xl bg-white shadow-sm">
+                    <h1 className="text-[1.8rem] font-bold">거래 정보</h1>
+                    <div className="flex flex-col gap-[1.4rem] mt-[1.8rem] border-b border-b-[#d6d6d6] pb-[1.4rem]">
+                        <div className="flex flex-row justify-between w-full items-center">
+                            <span className="text-[#4A5565] text-[1.4rem] font-normal">매수 가능 수량</span>
+                            <span className="text-black text-[1.6rem] font-bold">1주</span>
+                        </div>
+                        <div className="flex flex-row justify-between w-full items-center">
+                            <span className="text-[#4A5565] text-[1.4rem] font-normal">매도 가능 수량</span>
+                            <span className="text-black text-[1.6rem] font-bold">13,456원</span>
+                        </div>
+                    </div>
+                    <div className="py-[0.85rem] flex flex-row justify-between w-full items-center">
+                        <span className="text-[#4A5565] text-[1.4rem] font-normal">예수금</span>
+                        <span className="text-black text-[1.6rem] font-bold">1,223,456원</span>
+                    </div>
+                </div>
             </div>
-           
+            <div className="flex flex-row gap-[1.2rem] p-[2rem] bg-white w-full fixed bottom-0 z-10 rounded-t-[16px] border-t border-t-[#E5E7EB]">
+                <button className="py-[1.57rem] w-full bg-[#155DFC] text-white rounded-[16px] text-[1.6rem] font-bold">매도</button>
+                <button className="py-[1.57rem] w-full bg-[#E7000B] text-white rounded-[16px] text-[1.6rem] font-bold">매수</button>
+            </div>
         </div>
     )
 }
--- src/pages/order/components/OrderHeader.tsx
+++ src/pages/order/components/OrderHeader.tsx
@@ -1,9 +1,9 @@
 import BackIcon from '/icons/order/back.svg'
 const OrderHeader = () =>{
     return (
-        <div className="absolute top-0 left-0 w-full bg-white px-[1.2rem] py-[1.6rem] flex flex-row justify-start gap-[1.2rem] items-center">
+        <div className="absolute z-10 top-0 left-0 w-full bg-white px-[1.2rem] py-[0.8rem] flex flex-row justify-start gap-[1.2rem] items-center">
             <img src={BackIcon} className="w-[4rem] h-[4rem]"/>
-            <span className='text-[2rem] font-bold'>SK 하이닉스</span>
+            <span className='text-[1.7rem] font-bold'>SK 하이닉스</span>
         </div>
     )
 }
