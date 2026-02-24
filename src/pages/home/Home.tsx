4fb781b1e176e3e66de59f59b8b77b219b1c9603
jllee000 <jllee000@naver.com>
2026-02-24 21:22:45 +0900

    feat: 금액 포맷팅 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,10 +5,12 @@ import InfoBox from './components/InfoBox';
 import { useNavigate } from 'react-router-dom';
 import { INVEST_CHEERS } from '../../constants/investmentCheers';
 import { useState } from 'react';
+import MyAsset from '../assets/components/MyAsset';
 const Home =()=>{
     const navigate = useNavigate();
     const nickname = localStorage.getItem('nickname') || '투자왕님';
     const [randomCheer] = useState(() =>INVEST_CHEERS[Math.floor(Math.random() * INVEST_CHEERS.length)]);
+    const [myAsset, setMyAsset] = useState(10000000);
     return (
         <div className="pt-[7.2rem] pb-[8rem] px-[2rem] flex flex-col gap-[2.3rem] justify-between bg-[#F9FAFB]">
             <div className='flex flex-row justify-between items-center'>
@@ -25,7 +27,7 @@ const Home =()=>{
             <div className="p-[2.4rem] flex flex-col rounded-[24px] bg-gradient-to-br justify-between bg-gradient-to-br from-[#1447E6] to-[rgba(127,156,219,0.82)] shadow-[0_10px_15px_-3px_rgba(0,0,0,0.10),_0_4px_6px_-4px_rgba(0,0,0,0.10)]">
                 <span className="text-[#DBEAFE] text-[1.4rem]">💰 현재 내 자산</span>
                 <div className="flex flex-row items-center gap-2 pb-[1.2rem] border-b border-[#F3F4F6]">
-                    <span className="text-[#FFF] text-[3.6rem] font-bold">1000000</span>
+                    <span className="text-[#FFF] text-[3.6rem] font-bold">{myAsset.toLocaleString()}</span>
                     <span className="text-[#FFF] text-[2rem] font-normal">원</span>
                 </div>
                 <div className="flex flex-row justify-between items-center gap-2 pt-[1.6rem]">
